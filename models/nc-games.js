2b1b425cb3af07e911c2fb27cc3530712aa7968e
BaiDom <dombaily88@gmail.com>
2022-11-14 16:36:36 +0000

    models updated with fetchReviews and sql query to create comment_count and populate correctly

--- models/nc-games.js
+++ models/nc-games.js
@@ -1,7 +1,23 @@
 const db = require("../db/connection.js");
+const comments = require("../db/data/test-data/comments.js");
 
 exports.fetchCategories = () => {
-  return db.query("SELECT * FROM categories").then((result) => {
+  return db.query("SELECT * FROM categories;").then((result) => {
     return result.rows;
   });
+};
+
+exports.fetchReviews = () => {
+  return db
+    .query(
+      `
+       SELECT reviews.*, COUNT(comments.review_id):: INT AS comment_count
+       FROM reviews
+       LEFT JOIN comments ON reviews.review_id = comments.review_id
+       GROUP BY reviews.review_id
+       ORDER BY created_at DESC;`
+    )
+    .then((result) => {
+      return result.rows;
+    });
 };
