1bdf3e03bb86143c1d8d1564cab2bef9ed076f70
BaiDom <dombaily88@gmail.com>
2022-11-15 14:15:39 +0000

    endpoint to getCommentsByReviewId added to app.js

--- db/app.js
+++ db/app.js
@@ -3,6 +3,7 @@ const {
   getCategories,
   getReviews,
   getReviewsById,
+  getCommentsByReviewId,
 } = require("../controllers/nc-games");
 
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewsById);
+app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "Path not found" });
