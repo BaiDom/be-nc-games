12ad4f5ba9e353ae40501d1b74913772c1fc624e
BaiDom <dombaily88@gmail.com>
2022-11-15 16:56:05 +0000

    app.js updated for POST endpoint, app.use(express.json()) re-introduced

--- db/app.js
+++ db/app.js
@@ -4,15 +4,17 @@ const {
   getReviews,
   getReviewsById,
   getCommentsByReviewId,
+  postCommentByReviewId,
 } = require("../controllers/nc-games");
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id", getReviewsById);
 app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
+app.post("/api/reviews/:review_id/comments", postCommentByReviewId);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "Path not found" });
