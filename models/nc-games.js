7d32f81b228d0cffe413cd7a55bb11bfe1d55e6b
BaiDom <dombaily88@gmail.com>
2022-11-17 12:33:14 +0000

    fetchReviewsById refactored but be able to return a single review with comment count, also refactored to use checkReviewsExist util function

--- models/nc-games.js
+++ models/nc-games.js
@@ -24,17 +24,20 @@ exports.fetchReviews = () => {
 };
 
 exports.fetchReviewsById = (review_id) => {
-  return db
-    .query(
-      `
-    SELECT * FROM reviews WHERE review_id = $1;
-    `,
-      [review_id]
-    )
+  return checkReviewExists(review_id)
+    .then(() => {
+      return db.query(
+        `
+        SELECT reviews.*, COUNT(comments.review_id):: INT AS comment_count
+        FROM reviews
+        LEFT JOIN comments ON reviews.review_id = comments.review_id
+        WHERE reviews.review_id = $1
+        GROUP BY reviews.review_id
+        ORDER BY created_at DESC;`,
+        [review_id]
+      );
+    })
     .then((res) => {
-      if (res.rows.length === 0) {
-        return Promise.reject({ status: 400, msg: "Invalid review id" });
-      }
       return res.rows;
     });
 };
