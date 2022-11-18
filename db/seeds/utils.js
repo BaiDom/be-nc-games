84f64d7f57e55ace302b6869958e80ae2d0ea0aa
BaiDom <dombaily88@gmail.com>
2022-11-18 12:11:03 +0000

    removed check category exists func from utils because not needed

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -38,16 +38,6 @@ exports.checkReviewExists = (review_id) => {
     });
 };
 
-exports.checkCategoryExists = (category) => {
-  return db
-    .query(`SELECT * FROM categories WHERE slug = $1;`, [category])
-    .then((res) => {
-      if (res.rows.length === 0) {
-        return Promise.reject({ status: 400, msg: "Invalid category" });
-      }
-    });
-};
-
 exports.checkCommentExists = (comment_id) => {
   return db
     .query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
